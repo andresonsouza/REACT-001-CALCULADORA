3f3579781d235a4a44b4a565648bae4b02904fd3
Andreson Souza <andresonjazz@gmail.com>
2024-03-27 11:48:59 -0300

    configurações iniciais do projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import Calculator from "./components/Calculator";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-
-    </>
-  )
+  return <>
+    <Calculator/>
+  </>;
 }
 
-export default App
+export default App;
